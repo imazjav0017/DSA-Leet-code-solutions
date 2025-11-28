c12af6af9e235db2f43a86bdcdef0e56a07234f6
imazjav0017 <imazjaveed@gmail.com>
2025-11-28 12:34:07 +0000

    Time: 2 ms (98.54%) | Memory: 44.8 MB (36.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-      if(s.length()!=t.length())return false;
-        int[]arr=new int[26];
-        // Arrays.fill(arr,0);
-        for(char c:s.toCharArray())arr[c-'a']++;
-        for(char c:t.toCharArray()){
-            if(arr[c-'a']<1)return false;
-            arr[c-'a']--;
-        }
-        for(int val:arr)if(val!=0)return false;
-        return true;
-
+     if(s.length()!=t.length())
+        return false;
+     int n=s.length();
+     char[]s1=s.toCharArray();
+     char[]s2=t.toCharArray();
+     int []count=new int[26];
+     for (char c: s1){
+        count[c-'a']++;
+     }
+     for(char c: s2){
+        if(count[c-'a']<1)
+            return false;
+        count[c-'a']--;
+     }
+     for(int i:count){
+        if(i!=0)
+            return false;
+     }
+     return true;
     }
 }
