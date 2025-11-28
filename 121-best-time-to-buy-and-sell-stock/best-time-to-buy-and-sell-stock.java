791a32ca7021391c7cf1620a698ffd8104344d1a
imazjav0017 <imazjaveed@gmail.com>
2025-11-28 12:24:05 +0000

    Time: 1 ms (99.87%) | Memory: 94 MB (39.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy=Integer.MAX_VALUE,sell=0;
-        for(int i=0;i<prices.length;i++){
-            buy=Math.min(buy,prices[i]);
-            sell=Math.max(sell,prices[i]-buy);
-        }
+       int buy=prices[0],sell=0;
+       int n=prices.length;
+       for(int i=1;i<n;i++){
+        if(prices[i]<buy) //cheaper day to buy
+            buy=prices[i];
+        else if(prices[i]-buy>sell)
+            sell=prices[i]-buy;
+       }
        return sell;
+
     }
 }
