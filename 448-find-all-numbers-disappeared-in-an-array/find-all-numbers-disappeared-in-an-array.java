450af80384f1d3160065dcf2af8cbeb674b31473
imazjav0017 <imazjaveed@gmail.com>
2025-12-01 13:59:39 +0000

    Time: 6 ms (80.48%) | Memory: 67 MB (42.31%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,21 +1,27 @@
 class Solution {
-    /*
-    [4,3,2,7,8,2,3,1]
-    i=0->idx=4, a[3]=-1-> [4,3,2,-1,8,2,3,1]
-    i=1-> idx=3, a[2]=-1-> [4,3,-1,-1,8,2,3,1]
-    i=2-> idx=-1
-     */
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer>res=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++){ //[4,3,2,]
-           int idx=nums[i];
-           nums[Math.abs(idx)-1]=-(Math.abs(nums[Math.abs(idx)-1]));
+      List<Integer> list = new ArrayList<>();
+        int idx = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < 0){
+                idx = nums[i]*-1-1;
+            }else{
+                idx = nums[i]-1;
+            }
+            
+            if(nums[idx]>0){
+                nums[idx] = -nums[idx];
+            }
+            
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]>0)
-                res.add(i+1);
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                list.add(i+1);
+            };
+            
         }
-        return res;
+        
+        return list;
     }
 }
