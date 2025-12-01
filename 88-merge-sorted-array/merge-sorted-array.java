b311369a3c7f59f9be67303cb41c2cf5f9df7a79
imazjav0017 <imazjaveed@gmail.com>
2025-12-01 13:03:49 +0000

    Time: 0 ms (100.00%) | Memory: 43.8 MB (16.14%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,46 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(m==0){
-            for (int i = 0; i < n; i++) {
-            nums1[i] = nums2[i]; 
+        int p1 = m - 1;        // last valid element in nums1
+        int p2 = n - 1;        // last element in nums2
+        int p = m + n - 1;     // last position in nums1
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p--] = nums1[p1--];
+            } else {
+                nums1[p--] = nums2[p2--];
             }
         }
-        else if(n!=0)
-        {
-          int res[]=new int[m+n];
-          int p1=0;
-          int p2=0;
-          int filled=0;
-          for (;(p1<m && p2<n);filled++){
-            if(nums1[p1]<nums2[p2]){
-                res[filled]=nums1[p1];
-                p1++;
-            }
-            else
-            {
-                res[filled]=nums2[p2];
-                p2++;
-            }
-          }
-          if(p1<m){
-            int i=filled;
-            while(p1<m){
-                res[i]=nums1[p1];
-                p1++;
-                i++;
-            }
-          }
-          else if(p2<n){
-            int i=filled;
-            while(p2<n){
-                res[i]=nums2[p2];
-                p2++;
-                i++;
-            }
-          }
-          for (int i=0;i<m+n;i++){
-            nums1[i]=res[i];
-          }
+
+        // if nums2 still has elements, copy them
+        while (p2 >= 0) {
+            nums1[p--] = nums2[p2--];
         }
     }
 }
