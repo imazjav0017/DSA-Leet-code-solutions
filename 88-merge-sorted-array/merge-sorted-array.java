22152b4358a4369164f042f028c6f646948cf6eb
imazjav0017 <imazjaveed@gmail.com>
2025-12-01 13:05:55 +0000

    Time: 0 ms (100.00%) | Memory: 43.6 MB (32.99%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -6,9 +6,13 @@ class Solution {
 
         while (p1 >= 0 && p2 >= 0) {
             if (nums1[p1] > nums2[p2]) {
-                nums1[p--] = nums1[p1--];
+                nums1[p] = nums1[p1];
+                p--;
+                p1--;
             } else {
-                nums1[p--] = nums2[p2--];
+                nums1[p] = nums2[p2];
+                p--;
+                p2--;
             }
         }
 
