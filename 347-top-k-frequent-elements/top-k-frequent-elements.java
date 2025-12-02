f16a9c03cbb473aae80a3bf37986d4965c5ea9ca
imazjav0017 <imazjaveed@gmail.com>
2025-12-02 11:48:21 +0000

    Time: 12 ms (84.15%) | Memory: 52 MB (5.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -7,25 +7,53 @@ After processing everything, the heap contains the k most frequent elements.
 Extract them into the answer array.
  */
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int n : nums) {
-            freq.put(n, freq.getOrDefault(n, 0) + 1);
+        // Map<Integer, Integer> freq = new HashMap<>();
+        // for (int n : nums) {
+        //     freq.put(n, freq.getOrDefault(n, 0) + 1);
+        // }
+
+        // PriorityQueue<int[]> pq = new PriorityQueue<>(
+        //     (a, b) -> a[1] - b[1]     // min heap by frequency
+        // );
+
+        // for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
+        //     pq.offer(new int[]{e.getKey(), e.getValue()});
+        //     if (pq.size() > k) pq.poll();
+        // }
+
+        // int[] ans = new int[k];
+        // for (int i = k - 1; i >= 0; i--) {
+        //     ans[i] = pq.poll()[0];
+        // }
+
+        // return ans;
+        int n = nums.length;
+        Map<Integer, Integer> freqMap = new HashMap<>();  // num : freq
+        List<List<Integer>> buckets = new ArrayList<>();  // freq : [num1, num2...]
+        
+        for (int i = 0; i <= n; i++) {
+            buckets.add(new ArrayList<>());
         }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> a[1] - b[1]     // min heap by frequency
-        );
-
-        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
-            pq.offer(new int[]{e.getKey(), e.getValue()});
-            if (pq.size() > k) pq.poll();
+        
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-
-        int[] ans = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            ans[i] = pq.poll()[0];
+        
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            buckets.get(freq).add(num);
         }
-
-        return ans;
+        
+        int[] result = new int[k];
+        int idx = 0;
+        for (int i = n; i >= 0 && idx < k; i--) {
+            for (int num : buckets.get(i)) {
+                result[idx++] = num;
+                if (idx >= k) break;
+            }
+        }
+        
+        return result;
     }
 }
