492305dbf67ec39b5c49fbea4a8953e2ae470c04
imazjav0017 <imazjaveed@gmail.com>
2025-12-02 12:47:50 +0000

    Time: 6 ms (87.50%) | Memory: 43.9 MB (25.02%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,9 +3,6 @@ class Solution {
         int map1[]=new int[200];
         int map2[]=new int[200];
 
-        if(s.length()!=t.length())
-            return false;
-
         int n=s.length();
 
         for(int i=0;i<n;i++)
