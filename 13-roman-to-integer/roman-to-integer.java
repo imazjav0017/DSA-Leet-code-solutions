93180ba2610edc3fded73dc769de94f9446e394d
imazjav0017 <imazjaveed@gmail.com>
2025-12-02 13:50:20 +0000

    Time: 2 ms (100.00%) | Memory: 46.3 MB (37.71%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,39 +1,35 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character,Integer>map=new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
-        HashMap<String,Integer>valid=new HashMap<>();
-        valid.put("IV",4);
-        valid.put("IX",9);
-        valid.put("XL",40);
-        valid.put("XC",90);
-        valid.put("CD",400);
-        valid.put("CM",900);
-        int n=s.length();
-        if(n==1)return map.get(s.charAt(0));
-        int sum=0;
-        for(int i=0;i<n-1;i++){
-            char c1=s.charAt(i);
-            char c2=s.charAt(i+1);
-            char[]a={c1,c2};
-            String v=new String(a);
-            if(map.get(c1)>=map.get(c2)){
-                sum+=map.get(c1);
+       int ans = 0, num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
             }
-            else if(valid.containsKey(v))
-            {
-                sum+=valid.get(v);
-                i+=1;
-            }
-            else sum+=map.get(c1);
-            if(i==n-2)sum+=map.get(s.charAt(n-1));
+            if (4 * num < ans)
+                ans -= num;
+            else
+                ans += num;
         }
-        return sum;
+        return ans;
     }
 }
