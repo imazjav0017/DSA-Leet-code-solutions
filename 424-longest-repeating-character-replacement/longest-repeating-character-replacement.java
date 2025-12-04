f6181f3eda327ca817e5347a2d004c6da483437c
imazjav0017 <imazjaveed@gmail.com>
2025-12-04 15:58:19 +0000

    Time: 12 ms (48.92%) | Memory: 46.2 MB (26.62%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,24 +1,35 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int ans = 0;
-        int n = s.length();
-        for (char c = 'A'; c <= 'Z'; c++) {
-            int i = 0, j = 0, replaced = 0;
-            while (j < n) {
-                if (s.charAt(j) == c) {
-                    j++;
-                } else if (replaced < k) {
-                    j++;
-                    replaced++;
-                } else if (s.charAt(i) == c) {
-                    i++;
-                } else {
-                    i++;
-                    replaced--;
-                }
-                ans = Math.max(ans, j - i);
+        // Initialising an empty array to store the count of the 
+        // characters in the given string s
+        int[] arr = new int[26];
+        int res = 0;
+        int max = 0;
+
+        // The left pointer for the sliding window is l AND r is the 
+        // right pointer
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            // Counting the number of each character in the string s
+            arr[s.charAt(r) - 'A']++;
+
+            // Checking the character with max number of occurrence
+            max = Math.max(max, arr[s.charAt(r) - 'A']);
+
+            // Now we check if our current window is valid or not
+            if (r - l + 1 - max > k) { 
+            // this means the no. of replacements is more than
+            // allowed (k)
+                // Decrementing the count of the character which was 
+                // at l because it is no longer in the window
+                arr[s.charAt(l) - 'A']--;
+                l++;
             }
+
+            // The max our window can be
+            res = Math.max(res, r - l + 1);
         }
-        return ans;
+
+        return res;
     }
 }
