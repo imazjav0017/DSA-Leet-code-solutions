2020f11dc2dbdb7e2c201626d6a1ce917f526582
imazjav0017 <imazjaveed@gmail.com>
2025-12-04 15:24:21 +0000

    Time: 0 ms (100.00%) | Memory: 43.5 MB (15.35%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,28 +1,13 @@
 class Solution {
-    private void swap(int[]nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
     public int removeElement(int[] nums, int val) {
-        int n=nums.length;
-        int left=0, right=0;
-        while(right<n){
-            if(nums[right]!=val){
-                if(left!=right){
-                    swap(nums,left,right);
-                }
-                left++;
-                right++;
-            }
-            else {
-                if(left!=right && nums[left]!=val){
-                    swap(nums,left,right);
-                    left++;
-                }
-                right++;
+       int index = 0;
+       int n=nums.length;
+        for(int i = 0; i< n; i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return left;
+        return index;
     }
 }
