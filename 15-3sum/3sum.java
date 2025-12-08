672a3daf4793c132d443bb4602ccfecd21aecad4
imazjav0017 <imazjaveed@gmail.com>
2025-12-08 11:00:09 +0000

    Time: 1566 ms (5.00%) | Memory: 59.9 MB (12.33%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,28 @@
 class Solution {
+    /*
+    [-100,-70,-60,110,120,130,160]
+     */
     public List<List<Integer>> threeSum(int[] nums) {
-         List<List<Integer>> res = new ArrayList<>();
-        if (nums.length < 3) return res;
         int n=nums.length;
-
-        Arrays.sort(nums);// Sort array
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
-            if (nums[i] > 0) break; 
-
-            int left = i + 1, right = n - 1;
-            int target = -nums[i];
-
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                if (sum == target) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-
-                    // skip duplicates
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < target) {
-                    left++;
-                } else {
-                    right--;
+        Set<List<Integer>>res=new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<n-2;i++){
+            if(nums[i]>0)
+                break;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[left]+nums[right]+nums[i];
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
                 }
+                if(sum<0){
+                    left++;
+                }
+                else  right--;
             }
         }
-
-        return res;
+        List<List<Integer>> list = new ArrayList<>(res);
+        return list;
     }
 }
