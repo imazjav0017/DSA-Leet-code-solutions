0bb772f658b0008fdbb46b58ebb268fe974b3d8a
imazjav0017 <imazjaveed@gmail.com>
2025-12-08 09:45:40 +0000

    Time: 2 ms (100.00%) | Memory: 46.2 MB (51.65%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,45 @@
 class Solution {
-    public int romanToInt(String s) {
-       int ans = 0, num = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    num = 1;
-                    break;
-                case 'V':
-                    num = 5;
-                    break;
-                case 'X':
-                    num = 10;
-                    break;
-                case 'L':
-                    num = 50;
-                    break;
-                case 'C':
-                    num = 100;
-                    break;
-                case 'D':
-                    num = 500;
-                    break;
-                case 'M':
-                    num = 1000;
-                    break;
-            }
-            if (4 * num < ans)
-                ans -= num;
-            else
-                ans += num;
+    int getNumber(char c){
+        switch(c){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
         }
-        return ans;
+        return 0;
+    }
+    public int romanToInt(String s) {
+       int res=0;
+       int n=s.length();
+       boolean ended=false;
+       for(int i=0;i<n-1;i++){
+        char c=s.charAt(i);
+        char c1=s.charAt(i+1);
+        int n1=getNumber(c);
+        int n2=getNumber(c1);
+        if(n1>=n2){
+            res+=n1;
+        }
+        else{
+            res+=(n2-n1);
+            i++;
+            if(i==n-1)ended=true;
+        }
+       }
+       if(!ended){
+        int n1=getNumber(s.charAt(n-1));
+        res+=n1;
+       }
+       return res;
     }
 }
