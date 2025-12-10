db125905b56159f4fc0106d092d08279c1dd320a
imazjav0017 <imazjaveed@gmail.com>
2025-12-10 10:35:04 +0000

    Time: 4 ms (92.67%) | Memory: 77.1 MB (50.34%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
         int n=height.length;
-        int maxArea=0;
-        int start=0, end=n-1;
-        while(start<end){
-            //System.out.println("Start: "+start+" End: "+end);
-            int area=(end-start)*Math.min(height[start],height[end]);
-            //System.out.println("AREA: "+area+" Max Area:"+maxArea);
-            if(height[start]<height[end]){
-                start++;
-            }
-            else end--;
-            maxArea=Math.max(maxArea,area);
+        int max_area=0;
+        int left=0, right=n-1;
+        while(left<right){
+           int leftHeight = height[left], rightHeight = height[right];
+            int area = Math.min(leftHeight, rightHeight) * (right-left);
+            max_area = Math.max(area, max_area);
+
+            if(leftHeight < rightHeight)left++;
+            else right--;
         }
-        return maxArea;
+        return max_area;
     }
 }
