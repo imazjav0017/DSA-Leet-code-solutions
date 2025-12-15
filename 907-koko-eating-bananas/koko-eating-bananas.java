8e82836fbe4d0fe2240173b288a09034093724d3
imazjav0017 <imazjaveed@gmail.com>
2025-12-15 15:56:13 +0000

    Time: 25 ms (32.61%) | Memory: 47.9 MB (35.87%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,26 +1,31 @@
 class Solution {
-
-     public boolean canFinish(int[] piles, int h, int k) {
-        long hours = 0;
+    static int maxVal(int[] piles){
+        int ans=Integer.MIN_VALUE;
         for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
+            ans = Math.max(ans, pile);
         }
-        return hours <= h;
+        return ans;
     }
-    
+    static int totalHours(int[] arr, int mid){
+        int total=0;
+        for(int i=0;i<arr.length;i++){
+            total+=Math.ceil((double)arr[i]/(double)mid);
+        }
+        return (int)total;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;  
-        int right = Arrays.stream(piles).max().getAsInt();
-        int ans = right;
+        int left=1;
+        int right=maxVal(piles);
+        int ans=Integer.MAX_VALUE;
+        while(left<=right){
+            int mid=(left+right)/2;
+            int total=totalHours(piles,mid);
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                ans = mid;     
-                right = mid - 1;
-            } else {
-                left = mid + 1; 
+            if(total<=h){
+                ans=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
         }
         return ans;
