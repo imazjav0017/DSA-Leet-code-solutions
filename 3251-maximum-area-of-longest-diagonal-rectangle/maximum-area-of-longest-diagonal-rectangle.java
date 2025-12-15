943ae32cee92d889b8b353510b78345db3c09d4f
imazjav0017 <imazjaveed@gmail.com>
2025-12-15 21:15:02 +0000

    Time: 1 ms (96.42%) | Memory: 46.4 MB (44.63%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
@@ -1,31 +1,18 @@
 class Solution {
     public int areaOfMaxDiagonal(int[][] dimensions) {
-        int n=dimensions.length;
-        double[]diagonals=new double[n];
-        for(int i=0;i<n;i++){
-            int a= dimensions[i][0], b=dimensions[i][1];
-            double d=Math.sqrt((a*a + b*b));
-            diagonals[i]=d;
+         int n = dimensions.length;
+        int maxArea = 0, maxDiag = 0;
+
+        for (int i = 0; i < n; i++) {
+            int l = dimensions[i][0];
+            int w = dimensions[i][1];
+            int currDiag = l * l + w * w;
+
+            if (currDiag > maxDiag || (currDiag == maxDiag && l * w > maxArea)) {
+                maxDiag = currDiag;
+                maxArea = l * w;
+            }
         }
-        List<Integer>max=new ArrayList<>();
-        double maxD=0;
-        for(int i=0;i<n;i++){
-            if(diagonals[i]>maxD)
-                {
-                    maxD=diagonals[i];
-                    max.clear();
-                    max.add(i);
-                }
-            else if(diagonals[i]==maxD){
-                max.add(i);
-            }  
-        }
-    // System.out.println(max);
-    int maxArea=0;
-    for(int i=0;i<max.size();i++){
-        int area = dimensions[max.get(i)][0]*dimensions[max.get(i)][1];
-        if(area>maxArea)maxArea=area;
-    }
         return maxArea;
     }
 }
