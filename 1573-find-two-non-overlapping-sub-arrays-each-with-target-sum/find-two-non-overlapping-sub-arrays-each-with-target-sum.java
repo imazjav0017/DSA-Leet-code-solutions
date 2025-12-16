d86eaadd96a5ce6928dddea4933b5ec21f95e572
imazjav0017 <imazjaveed@gmail.com>
2025-12-16 11:55:01 +0000

    Time: 6 ms (97.35%) | Memory: 90.9 MB (56.95%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,20 +1,30 @@
 class Solution {
-    public int minSumOfLengths(int[] arr, int target) { //this fits the case when there's negative number, kind like 560
-        if (arr == null || arr.length == 0) return 0;
-        Map<Integer, Integer> map = new HashMap<>(); //sum - index
-        map.put(0, -1);
+    public int minSumOfLengths(int[] arr, int target) {
+        int[] min_len = new int[arr.length];
+        Arrays.fill(min_len, Integer.MAX_VALUE);
+        
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) { //record preSum and index
-            sum += arr[i];
-            map.put(sum, i);
+        int start = 0;
+        int res = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(int end = 0; end < arr.length; end++){
+            sum += arr[end];
+            
+            while(sum > target){
+                sum -= arr[start++];
+            }
+            
+            if(sum == target){
+                int cur_len = end-start+1;
+                if(start > 0 && min_len[start-1] != Integer.MAX_VALUE){
+                    res = Math.min(res, cur_len + min_len[start-1]);
+                }
+                min = Math.min(min, cur_len);
+            }
+            min_len[end] = min;
         }
-        sum = 0;
-        int size = arr.length + 1, res = arr.length + 1;//note if we set size as MAX_VALUE the line 16 may overflow
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-            if (map.containsKey(sum - target)) size = Math.min(size, i - map.get(sum - target)); //find the subarray from the previous index to current one
-            if (map.containsKey(sum + target)) res = Math.min(res, size + map.get(sum + target) - i); //from the current index to next one, this avoid overlap
-        }
-        return res == arr.length + 1 ? -1 : res;
+        
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
