ab8480eecd88908b60ba96c15f28263f3a5f016a
imazjav0017 <imazjaveed@gmail.com>
2025-12-22 16:40:35 +0000

    Time: 0 ms (100.00%) | Memory: 45 MB (64.51%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,71 +14,15 @@
  * }
  */
 class Solution {
-    public Integer min(Integer a, Integer b, Integer c, Integer d, Integer e){
-        if(b==null){
-            b=a;
-        }
-        if(c==null)
-        {
-            c=a;
-        }
-        if(d==null)d=a;
-        if(e==null)e=a;
-        if(a<=b && a<=c && a<=d && a<=e)
-            return a;
-        if(b<=a && b<=c && b<=d && b<=e)
-            return b;
-        if(c<=a && c<=b && c<=d && c<=e)
-            return c;
-        if(d<=a && d<=b && d<=c && d<=e)
-            return d;
-        return e;
+   public boolean isValid(TreeNode root,long left,long right){
+    if(root==null)
+        return true;
+    if(!(root.val<right && root.val>left)){
+        return false;
     }
-     public Integer max(Integer a, Integer b, Integer c, Integer d, Integer e){
-        if(b==null){
-            b=a;
-        }
-        if(c==null)
-        {
-            c=a;
-        }
-        if(d==null)d=a;
-        if(e==null)e=a;
-        if(a>=b && a>=c && a>=d && a>=e)
-            return a;
-        if(b>=a && b>=c && b>=d && b>=e)
-            return b;
-        if(c>=a && c>=b && c>=d && c>=e)
-            return c;
-        if(d>=a && d>=b && d>=c && d>=e)
-            return d;
-        return e;
-    }
-    public Integer[]isValid(TreeNode root){
-       if(root==null)
-       {
-        return new Integer[]{1,null,null};
-       }
-       Integer[]lhs=isValid(root.left);
-       Integer maxleft=lhs[1]==null?null:Math.max(lhs[1],lhs[2]);
-       Integer minleft=lhs[1]==null?null:Math.min(lhs[1],lhs[2]);
-       Integer[]rhs=isValid(root.right);
-       Integer minright=rhs[1]==null?null:Math.min(rhs[1],rhs[2]);
-       Integer maxright=rhs[1]==null?null:Math.max(rhs[1],rhs[2]);
-       boolean isV= lhs[0]==1 && (maxleft==null||maxleft<root.val)&& rhs[0]==1 
-       && (minright==null||minright>root.val);
-       Integer m1=min(root.val,
-       minleft,maxleft,minright,maxright);
-       Integer m2=max(root.val,minleft,maxleft,minright,maxright);
-    //    System.out.println(minleft+","+maxleft+","+minright+","+maxright);
-    //    System.out.println(m1+": "+m2);
-       Integer[]res=new Integer[]{isV?1:0,m1,m2};
-    //    System.out.println(root.val+":"+Arrays.toString(res));
-       return res;
-        
-    }
+    return isValid(root.left,left,root.val) && isValid(root.right,root.val,right);
+   }
     public boolean isValidBST(TreeNode root) {
-        Integer[]x=isValid(root);
-        return x[0]==1;
+        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
 }
