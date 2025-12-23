e3ca23e1269ab8b22024cfeb21e4c2d9cfd69a4a
imazjav0017 <imazjaveed@gmail.com>
2025-12-23 12:53:04 +0000

    Time: 7 ms (93.32%) | Memory: 46.7 MB (10.98%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,53 +8,18 @@
  * }
  */
 class Solution {
-    public List<TreeNode>findAncestors(TreeNode root,int k){
-          List<TreeNode> res= new ArrayList<>();
-          if(root==null){
-            return null;
-        }
-        if(root.val==k){
-            res.add(root);
-            return res;
-        }
-        List<TreeNode> left=findAncestors(root.left,k);
-        List<TreeNode> right=findAncestors(root.right,k);
-        if(left==null && right==null){
-            return null;
-        }
-        res.add(root);
-        if(left!=null){
-            for(TreeNode i:left){
-                res.add(i);
-            }
-        }
-        else if(right!=null){
-            for(TreeNode i:right){
-                res.add(i);
-            }
-        }
-        return res;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode>res1=findAncestors(root,p.val);
-        List<TreeNode>res2=findAncestors(root,q.val);
-        // for(TreeNode i: res1){
-        //     System.out.println(i.val+",");
-        // }
-        // System.out.println("________");
-        // for(TreeNode i: res2){
-        //     System.out.println(i.val+",");
-        // }
-        int min=Math.min(res1.size(),res2.size());
-        TreeNode lca=null;
-        for(int i=0;i<min && i<res1.size() && i<res2.size();i++){
-            if(res1.get(i).val==res2.get(i).val){
-                lca=res1.get(i);
-            }
-            else
-                break;
+        if(root==null)
+            return null;
+        if(root==p || root==q){
+            return root;
         }
-        return lca;
-        
+        TreeNode l=lowestCommonAncestor(root.left,p,q);
+        TreeNode r=lowestCommonAncestor(root.right,p,q);
+        if(l!=null && r!=null)
+            return root;
+        if(l!=null)
+            return l;
+        return r;
     }
 }
