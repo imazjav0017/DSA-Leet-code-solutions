d4c236ec3575479498ff6c95065674750eada02c
imazjav0017 <imazjaveed@gmail.com>
2025-12-23 19:02:50 +0000

    Time: 33 ms (36.22%) | Memory: 56 MB (10.46%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int first(String[]prods,String s, int start){
+        int n=prods.length;
+        if(start==-1)return -1;
+        int min=-1;
+        List<String>res=new ArrayList<>();
+        for(int i=start;i<n;i++){
+            if(prods[i].startsWith(s)){
+                min=i;
+                break;
+            }
+        }
+        return min;
+    }
+    public List<String>search(String[]prods,String s,int start){
+        int n=prods.length;
+        List<String>res=new ArrayList<>();
+        if(start==-1)return res;
+        for(int i=start;i<n;i++){
+            if(prods[i].startsWith(s)){
+                res.add(prods[i]);
+                if(res.size()==3)break;
+            }
+        }
+        return res;
+    }
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        List<List<String>>res=new ArrayList<>();
+        Arrays.sort(products);
+        //System.out.println(Arrays.toString(products));
+        int n=searchWord.length();
+        String s="";
+        int start=0;
+        for(int i=0;i<n;i++){
+            s+=searchWord.charAt(i);
+            start=first(products,s,start);
+            System.out.println(start);
+            List<String>list=search(products,s,start);
+            res.add(list);
+        }
+        return res;
+    }
+}
