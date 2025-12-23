cc9380c66ed49cc5c1854db310002c78c2dbd5f8
imazjav0017 <imazjaveed@gmail.com>
2025-12-23 14:07:16 +0000

    Time: 885 ms (3.64%) | Memory: 47.5 MB (7.81%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int findSum(List<Integer> x){
+        int sum=0;
+        for(int i:x){
+            sum+=i;
+        }
+        return sum;
+    }
+    List<List<Integer>> res=new ArrayList<>();
+    public void sum(TreeNode root,int currentSum, List<Integer>list,int targetSum){
+        if(root==null){
+            return;
+        }
+        currentSum+=root.val;
+        list.add(root.val);
+        System.out.println("_________root.val_____"+root.val);
+        for(int i:list){
+            System.out.println(i+",");
+        }
+        if(currentSum == targetSum && root.left==null && root.right==null){
+            res.add(new ArrayList<>(list));
+        }
+        else {
+            sum(root.left,currentSum,list,targetSum);
+            sum(root.right,currentSum,list,targetSum);
+        }
+        list.remove(list.size()-1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+         sum(root,0,new ArrayList<Integer>(),targetSum);
+         return res;
+    }
+}
