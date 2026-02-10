a234603cab06cbfa0261bc89645e118e01b4453a
imazjav0017 <imazjaveed@gmail.com>
2026-02-10 12:15:48 +0000

    Time: 1 ms (87.79%) | Memory: 47.3 MB (58.26%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,34 +1,33 @@
 class Solution {
-    HashSet<Integer>visited=new HashSet<Integer>();
-    public void bfs(int row,int rows, int cols, int[][]isConnected){
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<cols;i++){
-            if(i==row)
-                continue;
-            if(isConnected[row][i]==1){
-                q.add(i);
-            }
-        }
-        while(!q.isEmpty()){
-            int x=q.poll();
-            visited.add(x);
-            for(int i=0;i<cols;i++){
-                if(isConnected[x][i]==1 && !visited.contains(i)){
-                    q.add(i);
+    public void bfs(int [][] isConnected , int src, boolean[] isVisited){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(src);
+        isVisited[src] = true;
+        
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+
+            for(int i = 0; i < isConnected.length; i++){
+                if(isConnected[current][i] == 1 && !isVisited[i]){
+                    queue.offer(i);
+                    isVisited[i] = true;
                 }
             }
         }
     }
+    
+
     public int findCircleNum(int[][] isConnected) {
-        int rows=isConnected.length,cols=isConnected[0].length;
-        int res=0;
-        for(int i=0;i<rows;i++){
-            if(!visited.contains(i)){
-                res++;
-                bfs(i,rows,cols,isConnected);
-                visited.add(i);
+        int n = isConnected.length;
+        boolean[] isVisited = new boolean[n];
+        int Provinces = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!isVisited[i]){
+                Provinces++;              
+                bfs(isConnected, i, isVisited);
             }
         }
-        return res;
+        return Provinces;
     }
 }
