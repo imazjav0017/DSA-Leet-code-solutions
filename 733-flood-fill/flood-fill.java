0d46d43e8bd55ad224e53807714699b4712e193b
imazjav0017 <imazjaveed@gmail.com>
2026-02-11 12:14:02 +0000

    Time: 0 ms (100.00%) | Memory: 46.7 MB (78.68%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,25 +1,21 @@
 class Solution {
+    static int clr;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int src=image[sr][sc];
-        int rows=image.length,cols=image[0].length;
-        if(src==color)
-            return image;
-        Queue<int[]>queue=new ArrayDeque<>();
-        image[sr][sc]=color;
-        queue.offer(new int[]{sr,sc});
-        while(!queue.isEmpty()){
-            int[]x=queue.poll();
-            int r=x[0],c=x[1];
-            int top[]={r-1,c},bottom[]={r+1,c},left[]={r,c-1},right[]={r,c+1};
-            int directions[][]={top,bottom,left,right};
-            for(int[]dir:directions){
-                int dr=dir[0],dc=dir[1];
-                if(dr>=0 && dr<rows && dc>=0 && dc<cols && image[dr][dc]==src){
-                    queue.offer(dir);
-                    image[dr][dc]=color;
-                }
-            }
-        }
+        if (image.length==0) return image;
+        clr = image[sr][sc];
+        if(clr==color) return image;
+
+        dfs(image, sr, sc, color);
         return image;
+    }
+
+    public static void dfs(int[][] image, int sr, int sc, int color){
+        if(sr<0 || sc< 0|| sr>= image.length || sc>=image[0].length ||image[sr][sc]!=clr) return;
+        image[sr][sc] = color;
+        dfs(image, sr,sc-1,color);
+        dfs(image, sr,sc+1,color);
+        dfs(image, sr-1,sc,color);
+        dfs(image, sr+1,sc,color);
+        
     }
 }
