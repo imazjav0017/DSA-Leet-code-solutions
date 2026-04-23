c4f1ecc7f52a63682b138433d4c66119d1ef435c
imazjav0017 <imazjaveed@gmail.com>
2026-04-23 13:04:41 +0100

    Time: 12 ms (97.05%) | Memory: 48.1 MB (25.15%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -1,68 +1,41 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        char[]arr=start.toCharArray();
-        char[]tgt=target.toCharArray();
-        int n=tgt.length;
-        List<Integer>lefts=new ArrayList<>();
-        List<Integer>rights=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            char c=tgt[i];
-            if(c=='L')
-                lefts.add(i);
-            else if(c=='R')
-                rights.add(i);
+        if (start.equals(target)) {
+            return true;
         }
-        int lr=0;
-        for(int ti:lefts){
-            if(!(arr[ti]=='L'||arr[ti]=='_'))
-                return false;
-            lr=ti;
-            while(lr<n){
-                if(arr[lr]=='L')
-                    {
-                        break;
-                    }
-                else if(arr[lr]=='R')
+        int waitL = 0;
+        int waitR = 0;
+        
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start.charAt(i);
+            char goal = target.charAt(i);
+            
+            if (curr == 'R') {
+                if (waitL > 0) {
                     return false;
-                lr++;
-            }
-            if(lr<n)
-                {
-                    arr[ti]='L';
-                    if(ti!=lr)arr[lr]='_';
-                    lr++;
                 }
-            else
-                return false;
+                waitR++;
+            }
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
         }
-        // System.out.println(new String(arr));
-        int rs=rights.size();
-        int rr=n-1;
-        for(int t=rs-1;t>=0;t--){
-            int ti=rights.get(t);
-            if(!(arr[ti]=='R'||arr[ti]=='_'))
-                return false;
-            rr=ti;
-            while(rr>=0){
-                if(arr[rr]=='R')
-                    {
-                        break;
-                    }
-                else if(arr[rr]=='L')
-                    return false;
-                rr--;
-            }
-            if(rr>=0)
-                {
-                    arr[ti]='R';
-                    if(ti!=rr)arr[rr]='_';
-                    rr--;
-                }
-            else
-                return false;
-            } 
-    
-    // System.out.println(new String(arr));
-    return target.equals(new String(arr));
+        
+        return waitL == 0 && waitR == 0;
     }
 }
