f8daf2b8685b0a4c94a3bf639f03281be23e83f9
imazjav0017 <imazjaveed@gmail.com>
2026-04-23 14:03:47 +0100

    Time: 13 ms (91.55%) | Memory: 47.8 MB (78.19%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -1,25 +1,38 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        int left = 0, right = 0;
+        int i = 0, j = 0;
+        int n = start.length();
 
-        for (int i = 0; i < start.length(); i++) {
-            if (start.charAt(i) == 'R') {
-                right++;
-                if (left != 0)
-                    return false;
-            } else if (start.charAt(i) == 'L') {
-                left--;
-            }
-            if (target.charAt(i) == 'R') {
-                right--;
-            } else if (target.charAt(i) == 'L') {
-                left++;
-                if (right != 0)
-                    return false;
-            }
-            if (left < 0 || right < 0)
-                return false;
+        while (i < n || j < n) {
+
+            // skip blanks in start
+            while (i < n && start.charAt(i) == '_') i++;
+
+            // skip blanks in target
+            while (j < n && target.charAt(j) == '_') j++;
+
+            // both finished
+            if (i == n && j == n) return true;
+
+            // one finished early
+            if (i == n || j == n) return false;
+
+            char a = start.charAt(i);
+            char b = target.charAt(j);
+
+            // pieces must match
+            if (a != b) return false;
+
+            // L cannot move right
+            if (a == 'L' && i < j) return false;
+
+            // R cannot move left
+            if (a == 'R' && i > j) return false;
+
+            i++;
+            j++;
         }
-        return left == 0 && right == 0;
+
+        return true;
     }
 }
