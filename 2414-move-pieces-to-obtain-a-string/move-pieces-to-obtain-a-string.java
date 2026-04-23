5b66cb5bec1267b87134c139e11adceeb4de4730
imazjav0017 <imazjaveed@gmail.com>
2026-04-23 13:09:34 +0100

    Time: 17 ms (26.33%) | Memory: 48 MB (25.15%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -1,41 +1,25 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        if (start.equals(target)) {
-            return true;
-        }
-        int waitL = 0;
-        int waitR = 0;
-        
+        int left = 0, right = 0;
+
         for (int i = 0; i < start.length(); i++) {
-            char curr = start.charAt(i);
-            char goal = target.charAt(i);
-            
-            if (curr == 'R') {
-                if (waitL > 0) {
+            if (start.charAt(i) == 'R') {
+                right++;
+                if (left != 0)
                     return false;
-                }
-                waitR++;
+            } else if (start.charAt(i) == 'L') {
+                left--;
             }
-            if (goal == 'L') {
-                if (waitR > 0) {
+            if (target.charAt(i) == 'R') {
+                right--;
+            } else if (target.charAt(i) == 'L') {
+                left++;
+                if (right != 0)
                     return false;
-                }
-                waitL++;
             }
-            if (goal == 'R') {
-                if (waitR == 0) {
-                    return false;
-                }
-                waitR--;
-            }
-            if (curr == 'L') {
-                if (waitL == 0) {
-                    return false;
-                }
-                waitL--;
-            }
+            if (left < 0 || right < 0)
+                return false;
         }
-        
-        return waitL == 0 && waitR == 0;
+        return left == 0 && right == 0;
     }
 }
