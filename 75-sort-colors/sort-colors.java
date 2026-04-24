49679490b7382dcc0aa5aec98df92832028295b9
imazjav0017 <imazjaveed@gmail.com>
2026-04-24 10:22:55 +0100

    Time: 0 ms (100.00%) | Memory: 42.6 MB (99.79%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,19 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0;              // next position for 0
-        int mid = 0;              // current scanner
-        int high = nums.length - 1; // next position for 2
-
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums, low, mid);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else { // nums[mid] == 2
-                swap(nums, mid, high);
-                high--;
+        int write0=0,write2=nums.length-1;
+        int read=0;
+        while(read<=write2){
+            if(nums[read]==0){
+                swap(nums,read,write0);
+                write0++;
+                read++;
+            }
+            else if(nums[read]==1){
+                read++;
+            }
+            else{
+                swap(nums,read,write2);
+                write2--;
             }
         }
     }
