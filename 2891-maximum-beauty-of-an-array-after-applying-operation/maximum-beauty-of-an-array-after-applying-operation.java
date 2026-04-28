ffee9a507e295b2ba68a93ab09bc3546f31f1317
imazjav0017 <imazjaveed@gmail.com>
2026-04-28 15:26:14 +0100

    Time: 41 ms (88.10%) | Memory: 89.7 MB (92.86%) - LeetSync

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int low=nums[0]-k;
+        int high=nums[0]+k;
+        int left=0,right=0;
+        int maxLen=0;
+        while(right<n){
+            low=nums[right]-k;
+            while(low>high){
+                left++;
+                low=nums[left]-k;
+                high=nums[left]+k;
+            }
+            maxLen=Math.max(maxLen,right-left+1);
+            right++;
+        }
+        return maxLen;
+    }
+}
