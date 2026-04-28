987952f42a5d3da32191717b1b7d88c3291987df
imazjav0017 <imazjaveed@gmail.com>
2026-04-28 09:27:15 +0100

    Time: 49 ms (86.58%) | Memory: 66.5 MB (93.85%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -7,7 +7,7 @@ class Solution {
         while(right<n){
             int c=map.getOrDefault(fruits[right],0);
             map.put(fruits[right],c+1);
-            while(left<=right && map.keySet().size()>2){
+            while(left<=right && map.size()>2){
                 int cl=map.getOrDefault(fruits[left],0)-1;
                 if(cl<1){
                     map.remove(fruits[left]);
