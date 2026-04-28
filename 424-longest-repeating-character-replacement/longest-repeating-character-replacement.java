6cb21fafa430c46387963bc9cf20b995b3f2517b
imazjav0017 <imazjaveed@gmail.com>
2026-04-28 13:02:49 +0100

    Time: 12 ms (46.39%) | Memory: 46.2 MB (64.76%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,35 +1,28 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // Initialising an empty array to store the count of the 
-        // characters in the given string s
-        int[] arr = new int[26];
-        int res = 0;
-        int max = 0;
-
-        // The left pointer for the sliding window is l AND r is the 
-        // right pointer
-        int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            // Counting the number of each character in the string s
-            arr[s.charAt(r) - 'A']++;
-
-            // Checking the character with max number of occurrence
-            max = Math.max(max, arr[s.charAt(r) - 'A']);
-
-            // Now we check if our current window is valid or not
-            if (r - l + 1 - max > k) { 
-            // this means the no. of replacements is more than
-            // allowed (k)
-                // Decrementing the count of the character which was 
-                // at l because it is no longer in the window
-                arr[s.charAt(l) - 'A']--;
-                l++;
+        int n=s.length();
+        int[]count=new int[26];
+        int left=0,right=0;
+        int maxLen=0,maxCount=0;
+        while(right<n){
+            char c=s.charAt(right);
+            count[c-'A']++;
+            for(int i=0;i<26;i++){
+                maxCount=Math.max(maxCount,count[i]);
             }
-
-            // The max our window can be
-            res = Math.max(res, r - l + 1);
+            int len=right-left+1;
+            while(len-maxCount>k){
+                char cl=s.charAt(left);
+                count[cl-'A']--;
+                for(int i=0;i<26;i++){
+                maxCount=Math.max(maxCount,count[i]);
+                }
+                left++;
+                len=right-left+1;
+            }
+            maxLen=Math.max(len,maxLen);
+            right++;
         }
-
-        return res;
+        return maxLen;
     }
 }
