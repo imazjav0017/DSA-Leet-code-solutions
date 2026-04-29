f9537256598b2b9e5fb18178d1fed860a0106a8d
imazjav0017 <imazjaveed@gmail.com>
2026-04-29 10:59:00 +0100

    Time: 2 ms (99.95%) | Memory: 45.3 MB (90.90%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,57 +1,48 @@
 class Solution {
-    boolean valid(Map<Character,Integer>source,Map<Character,Integer>target){
-        for(char c:target.keySet()){
-            int count1=target.get(c);
-            int count2=source.getOrDefault(c,0);
-            //System.out.println("C:"+c+" C1: "+count1+" C2: "+count2);
-            if(count1>count2)
-                return false;
-        }
-        return true;
-    }
     public String minWindow(String s, String t) {
-        int m=s.length();
-        int n=t.length();
-        if(m<n)
-            return "";
-        Map<Character,Integer>targetMap=new HashMap<>();
-        for(int i=0;i<n;i++){
-            char c=t.charAt(i);
-            int count=targetMap.getOrDefault(c,0);
-            targetMap.put(c,count+1);
+        if (s.length() < t.length()) return "";
+
+        int[] need = new int[128];
+        int required = t.length();
+
+        for (char c : t.toCharArray()) {
+            need[c]++;
         }
-        int[]res=new int[2];
-        int minLen=m+1;
-        int left=0,right=0;
-        Map<Character,Integer>sourceMap=new HashMap<>();
-        // for(char c:targetMap.keySet()){
-        //     System.out.println(c+" : "+targetMap.get(c));
-        // }
-        while(right<m){
-            char c=s.charAt(right);
-           //System.out.println("c:"+c);
-            if(targetMap.getOrDefault(c,0)!=0){
-                sourceMap.put(c,sourceMap.getOrDefault(c,0)+1);
-                //  for(char c1:sourceMap.keySet()){
-                //     System.out.println(c1+" : "+sourceMap.get(c1));
-                //     }
+
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (need[c] > 0) {
+                required--;
             }
-            //System.out.println("Check: "+s.substring(left,right+1));
-            while(valid(sourceMap,targetMap)){
-                int len=right-left+1;
-                //System.out.println("Check Pass: "+s.substring(left,right+1));
-                minLen=Math.min(minLen,len);
-                if(minLen==len){
-                    res[0]=left;
-                    res[1]=right;
+
+            need[c]--;
+
+            while (required == 0) {
+                int len = right - left + 1;
+
+                if (len < minLen) {
+                    minLen = len;
+                    start = left;
                 }
-                char cl=s.charAt(left);
-                if(targetMap.containsKey(cl))
-                    sourceMap.put(cl,sourceMap.get(cl)-1);
+
+                char leftChar = s.charAt(left);
+                need[leftChar]++;
+
+                if (need[leftChar] > 0) {
+                    required++;
+                }
+
                 left++;
             }
-            right++;
         }
-        return minLen<=m?s.substring(res[0],res[1]+1):"";
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
     }
 }
