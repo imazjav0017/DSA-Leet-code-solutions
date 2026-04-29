59b530f0b7092832118125a3cf6b715838495909
imazjav0017 <imazjaveed@gmail.com>
2026-04-29 12:35:11 +0100

    Time: 18 ms (85.54%) | Memory: 86.3 MB (54.86%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        int n=nums.length;
+        Deque<Integer>max=new ArrayDeque<>();
+        Deque<Integer>min=new ArrayDeque<>();
+        int left=0,right=0;
+        long total=0;
+        while(right<n){
+            int num=nums[right];
+            while(!max.isEmpty() && max.peekLast()<num){
+                max.pollLast();
+            }
+            max.offerLast(num);
+            while(!min.isEmpty() && min.peekLast()>num){
+                min.pollLast();
+            }
+            min.offerLast(num);
+            while(max.peekFirst()-min.peekFirst()>2){
+                if(max.peekFirst()==nums[left]){
+                    max.pollFirst();
+                }
+                if(min.peekFirst()==nums[left]){
+                    min.pollFirst();
+                }
+                left++;
+            }
+            
+            total+= right - left + 1;;
+            right++;
+        }
+        return total;
+    }
+}
