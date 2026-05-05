47ac7bc4c7552a1f0993110ca7dd29c303f4d0c5
imazjav0017 <imazjaveed@gmail.com>
2026-05-05 11:00:43 +0100

    Time: 82 ms (79.62%) | Memory: 126.9 MB (61.61%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,48 +1,37 @@
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        Arrays.sort(nums);
-
-        int n = nums.length;
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        }
-
-        int ans = 1;
-
-        int left = 0;
-        int right = 0;
-
-        for (int i = 0; i < n; i++) {
-            int target = nums[i];
-
-            while (left < n && nums[left] < target - k) {
-                left++;
-            }
-
-            while (right < n && nums[right] <= target + k) {
-                right++;
-            }
-
-            int reachable = right - left;
-            int alreadyEqual = freq.get(target);
-            int canChange = reachable - alreadyEqual;
-
-            ans = Math.max(ans, alreadyEqual + Math.min(numOperations, canChange));
-        }
-
-        left = 0;
-
-        for (right = 0; right < n; right++) {
-            while (nums[right] - nums[left] > 2 * k) {
-                left++;
-            }
-
-            int windowSize = right - left + 1;
-            ans = Math.max(ans, Math.min(numOperations, windowSize));
-        }
-
-        return ans;
+    int n=nums.length;
+    Arrays.sort(nums);
+    Map<Integer,Integer>freqMap=new HashMap<>();
+    for(int i:nums){
+        freqMap.put(i,freqMap.getOrDefault(i,0)+1);
     }
+    int left=0;
+    int right=0;
+    int ans=0;
+    //target is in array
+    for(int i=0;i<n;i++){
+        int target=nums[i];
+        while(left<n && nums[left]<target-k){
+            left++;
+        }
+        while(right<n && nums[right]<=target+k){
+            right++;
+        }
+        int reachable=right-left;
+        int alreadyEqual=freqMap.get(target);
+        int canChange=reachable-alreadyEqual;
+        ans=Math.max(ans,alreadyEqual+Math.min(numOperations,canChange));
+    }
+    //target is not in array
+    left=0;
+    for(right=0;right<n;right++){
+        while(nums[right]-nums[left]>2*k){
+            left++;
+        }
+        int win=right-left+1;
+        ans=Math.max(ans,Math.min(numOperations,win));
+    }
+    return ans;
+}
 }
