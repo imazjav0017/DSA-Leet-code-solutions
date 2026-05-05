fa7e453d42b5e94215eea94ce3b46806acd48dfd
imazjav0017 <imazjaveed@gmail.com>
2026-05-05 10:42:05 +0100

    Time: 14 ms (72.86%) | Memory: 46.7 MB (55.39%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
@@ -1,41 +1,35 @@
 class Solution {
     public int takeCharacters(String s, int k) {
-        int n=s.length();
-        Map<Character,Integer>freq=new HashMap<>();
-        freq.put('a',0);
-        freq.put('b',0);
-        freq.put('c',0);
-        for(char c:s.toCharArray()){
-            freq.put(c,freq.get(c)+1);
+        int n = s.length();
+        int[] total = new int[3];
+
+        for (char c : s.toCharArray()) {
+            total[c - 'a']++;
         }
-        int aToRemove=freq.get('a')-k;
-        int bToRemove=freq.get('b')-k;
-        int cToRemove=freq.get('c')-k;
-        if(aToRemove<0 ||bToRemove<0 ||cToRemove<0)
-            return -1;
-        int maxLen=0;
-        int left=0,right=0;
-        Map<Character,Integer>windowMap=new HashMap<>();
-        windowMap.put('a',0);
-        windowMap.put('b',0);
-        windowMap.put('c',0);
-        while(right<n){
-            char c=s.charAt(right);
-            windowMap.put(c,windowMap.get(c)+1);
-            while(left<n && (windowMap.get('a')>aToRemove 
-            ||windowMap.get('b')>bToRemove || windowMap.get('c')>cToRemove)){
-                char cl=s.charAt(left);
-                windowMap.put(cl,windowMap.get(cl)-1);
+
+        for (int count : total) {
+            if (count < k) return -1;
+        }
+
+        int[] window = new int[3];
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < n; right++) {
+            window[s.charAt(right) - 'a']++;
+
+            while (
+                window[0] > total[0] - k ||
+                window[1] > total[1] - k ||
+                window[2] > total[2] - k
+            ) {
+                window[s.charAt(left) - 'a']--;
                 left++;
             }
-            if(right<left){
-                right=left;
-            }
-            else{
-                maxLen=Math.max(maxLen,right-left+1);
-                right++;
-            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
         }
-        return n-maxLen;
+
+        return n - maxLen;
     }
 }
