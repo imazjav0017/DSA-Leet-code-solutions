edd280c37e32d50197c9988939fcbf0e5c2f4e2e
imazjav0017 <imazjaveed@gmail.com>
2026-05-09 19:54:21 +0100

    Time: 1 ms (71.45%) | Memory: 44 MB (13.92%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,24 +1,39 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                // last node at this level
-                if (i == size - 1) res.add(node.val);
-                // add kids
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
+        List<Integer>res=new ArrayList<>();
+        Queue<TreeNode> q=new ArrayDeque<>();
+        if(root==null)
+            return res;
+        q.add(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode x=q.poll();
+                if(x.left!=null)
+                    q.offer(x.left);
+                if(x.right!=null)
+                    q.offer(x.right);
+                if(i==n-1){
+                    res.add(x.val);
+                }
             }
         }
         return res;
+
     }
 }
