440456ad0ff79829080312b1d104fbff8c6656fa
imazjav0017 <imazjaveed@gmail.com>
2026-05-11 10:01:17 +0100

    Time: 3 ms (67.30%) | Memory: 46.1 MB (93.15%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,35 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    String serialize(TreeNode root,StringBuilder builder){
-        if(root==null)
-            {
-                builder.append("#,");
-                return builder.toString();
-            }
-        builder.append("@"+root.val+",");
-        serialize(root.left,builder);
-        serialize(root.right,builder);
-        return builder.toString();
-    }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        StringBuilder b1=new StringBuilder();
-        StringBuilder b2=new StringBuilder();
-        String subString=serialize(subRoot,b1);
-        String str=serialize(root,b2);
-        return str.contains(subString);
+        if (root == null) return false;
+
+        if (isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) 
+            || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+
+        if (a.val != b.val) return false;
+
+        return isSameTree(a.left, b.left) 
+            && isSameTree(a.right, b.right);
     }
 }
