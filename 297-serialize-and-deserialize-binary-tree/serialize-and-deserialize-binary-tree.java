844f416616a8f103998f6034b204db3d08cffcb7
imazjav0017 <imazjaveed@gmail.com>
2026-05-11 10:28:01 +0100

    Time: 7 ms (96.93%) | Memory: 48.4 MB (28.65%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -1,48 +1,39 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Codec {
-    StringBuilder build=new StringBuilder();
-    // Encodes a tree to a single string.
+
     public String serialize(TreeNode root) {
-        if(root==null){
-            build.append("#,");
-            return build.toString();
-        }
-        build.append(root.val).append(",");
-        serialize(root.left);
-        serialize(root.right);
+        StringBuilder build = new StringBuilder();
+        serializeHelper(root, build);
         return build.toString();
     }
 
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        String[]tokens=data.split(",");
-        int[]index=new int[1];
-        TreeNode root=deserializeHelper(tokens,index);
-        return root;
+    private void serializeHelper(TreeNode root, StringBuilder build) {
+        if (root == null) {
+            build.append("#,");
+            return;
+        }
+
+        build.append(root.val).append(",");
+        serializeHelper(root.left, build);
+        serializeHelper(root.right, build);
     }
-    private TreeNode deserializeHelper(String[]tokens,int[]index){
-        if(index[0]>=tokens.length){
+
+    public TreeNode deserialize(String data) {
+        String[] tokens = data.split(",");
+        int[] index = new int[1];
+        return deserializeHelper(tokens, index);
+    }
+
+    private TreeNode deserializeHelper(String[] tokens, int[] index) {
+        String token = tokens[index[0]++];
+
+        if (token.equals("#")) {
             return null;
         }
-        String token=tokens[index[0]++];
-        if(token.equals("#"))
-            return null;
-        TreeNode root=new TreeNode(Integer.parseInt(token));
-        root.left=deserializeHelper(tokens,index);
-        root.right=deserializeHelper(tokens,index);
+
+        TreeNode root = new TreeNode(Integer.parseInt(token));
+        root.left = deserializeHelper(tokens, index);
+        root.right = deserializeHelper(tokens, index);
+
         return root;
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
