6f0d9a851671208350c231d71a82721bc91add07
imazjav0017 <imazjaveed@gmail.com>
2026-05-14 14:36:49 +0100

    Time: 0 ms (100.00%) | Memory: 42.9 MB (95.45%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
@@ -14,40 +14,15 @@
  * }
  */
 class Solution {
-    boolean dfs(TreeNode root1,TreeNode root2){
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
         if(root1==null && root2==null)
             return true;
         if(root1==null ||root2==null)
             return false;
         if(root1.val!=root2.val)
             return false;
-        TreeNode left1=root1.left,left2=root2.left;
-        TreeNode right1=root1.right,right2=root2.right;
-        boolean flip =
-    ((left1 == null && right2 == null) ||
-     (left1 != null && right2 != null && left1.val == right2.val))
-    &&
-    ((right1 == null && left2 == null) ||
-     (right1 != null && left2 != null && right1.val == left2.val));
-
-boolean noFlip =
-    ((left1 == null && left2 == null) ||
-     (left1 != null && left2 != null && left1.val == left2.val))
-    &&
-    ((right1 == null && right2 == null) ||
-     (right1 != null && right2 != null && right1.val == right2.val));
-        if(!flip && !noFlip)
-            return false;
-        if(flip){
-            return dfs(left1,right2) &&
-            dfs(right1,left2);
-        }
-        else{
-            return dfs(left1,left2) &&
-            dfs(right1,right2);
-        }
-    }
-    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        return dfs(root1,root2);
+        boolean flip= flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left);
+        boolean noFlip= flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right);
+        return flip || noFlip;
     }
 }
