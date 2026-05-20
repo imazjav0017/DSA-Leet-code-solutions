c112825a0a794f70b758cd794f50e2cd706a1f53
imazjav0017 <imazjaveed@gmail.com>
2026-05-20 17:53:23 +0100

    Time: 2 ms (75.83%) | Memory: 47.5 MB (84.93%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,35 +1,37 @@
 class Solution {
-    private void dfs(int r,int c,char[][]board,boolean[][] visited){
-        if(r<0||c<0 ||r>=board.length||c>=board[0].length || board[r][c]=='X'|| visited[r][c])
-            return;
-        visited[r][c]=true;
-        dfs(r-1,c,board,visited);
-        dfs(r+1,c,board,visited);
-        dfs(r,c+1,board,visited);
-        dfs(r,c-1,board,visited);
+    int[][]dirs={{0,1},{0,-1},{1,0},{-1,0}};
+    void dfs(char[][]board,int row, int col, int m, int n,boolean[][]visited){
+        visited[row][col]=true;
+        for(int[]d:dirs){
+            int nr=row+d[0];
+            int nc=col+d[1];
+            if(nr<0||nr>=m||nc<0||nc>=n)
+                continue;
+            if(visited[nr][nc] || board[nr][nc]=='X')
+                continue;
+            dfs(board,nr,nc,m,n,visited);
+        }
     }
     public void solve(char[][] board) {
-        int m=board.length,n=board[0].length;
-        boolean[][]visited=new boolean[m][n];
-        for(int j=0;j<n;j++){
-            if(board[0][j]=='O' && !visited[0][j])
-                dfs(0,j,board,visited);
+        int m=board.length;
+        int n=board[0].length;
+        boolean[][]safe=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            if(!safe[i][0] && board[i][0]=='O')
+                dfs(board,i,0,m,n,safe);
+            if(!safe[i][n-1] && board[i][n-1]=='O')
+                dfs(board,i,n-1,m,n,safe);
         }
         for(int j=0;j<n;j++){
-            if(board[m-1][j]=='O' && !visited[m-1][j])
-                dfs(m-1,j,board,visited);
-        }
-        for(int j=0;j<m;j++){
-            if(board[j][0]=='O' && !visited[j][0])
-                dfs(j,0,board,visited);
-        }
-        for(int j=0;j<m;j++){
-            if(board[j][n-1]=='O' && !visited[j][n-1])
-                dfs(j,n-1,board,visited);
+            //System.out.println("CH:"+board[m-1][j]+","+String.valueOf(board[m-1][j]=='O'));
+            if(!safe[0][j] && board[0][j]=='O')
+                dfs(board,0,j,m,n,safe);
+            if(!safe[m-1][j] && board[m-1][j]=='O')
+                dfs(board,m-1,j,m,n,safe);
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!visited[i][j] && board[i][j]=='O')
+                if(board[i][j]=='O' && !safe[i][j])
                     board[i][j]='X';
             }
         }
