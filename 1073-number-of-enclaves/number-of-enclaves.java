abd5ee02535d3e5b858ac6cb27e030e547f3238c
imazjav0017 <imazjaveed@gmail.com>
2026-05-21 15:01:34 +0100

    Time: 8 ms (70.39%) | Memory: 61.7 MB (23.46%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,39 @@
+class Solution {
+    int[][]dirs={{0,1},{0,-1},{1,0},{-1,0}};
+    void dfs(int[][]grid,int m, int n, int row, int col, boolean[][]visited){
+        visited[row][col]=true;
+        for(int[]d:dirs){
+            int nr=row+d[0],nc=col+d[1];
+            if(nr<0||nr>=m||nc<0||nc>=n)
+                continue;
+            if(visited[nr][nc] || grid[nr][nc]==0)
+                continue;
+            dfs(grid,m,n,nr,nc,visited);
+        }
+    }
+    public int numEnclaves(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean[][]visited=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1 && !visited[i][0])
+                dfs(grid,m,n,i,0,visited);
+            if(grid[i][n-1]==1 && !visited[i][n-1])
+                dfs(grid,m,n,i,n-1,visited);
+        }
+        for(int j=0;j<n;j++){
+            if(grid[0][j]==1 && !visited[0][j])
+                dfs(grid,m,n,0,j,visited);
+            if(grid[m-1][j]==1 && !visited[m-1][j])
+                dfs(grid,m,n,m-1,j,visited);
+        }
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && !visited[i][j])
+                    count++;
+            }
+        }
+        return count;
+    }
+}
