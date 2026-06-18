ca6f67a59f2712751079ad2246a886bc982344a8
imazjav0017 <imazjaveed@gmail.com>
2026-06-18 14:50:31 +0100

    Time: 16 ms (28.37%) | Memory: 49.3 MB (33.49%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,35 +1,37 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        if (mat == null || mat.length == 0 || mat[0].length == 0)
-            return new int[0][0];
-
-        int m = mat.length, n = mat[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        int MAX_VALUE = m * n;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                } else {
-                    mat[i][j] = MAX_VALUE;
+        int m=mat.length,n=mat[0].length;
+        int[][]res=new int[m][n];
+        Queue<int[]>queue=new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    queue.offer(new int[]{i,j});
+                }
+                else{
+                    res[i][j]=-1;
                 }
             }
         }
-        
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] dir : directions) {
-                int r = cell[0] + dir[0], c = cell[1] + dir[1];
-                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[cell[0]][cell[1]] + 1) {
-                    queue.offer(new int[]{r, c});
-                    mat[r][c] = mat[cell[0]][cell[1]] + 1;
+        int[][]dirs={{0,1},{0,-1},{1,0},{-1,0}};
+        int d=1;
+        while(!queue.isEmpty()){
+            int s=queue.size();
+            for(int i=0;i<s;i++){
+                int[]point=queue.poll();
+                int r=point[0],c=point[1];
+                for(int[]dir:dirs){
+                    int nr=r+dir[0],nc=c+dir[1];
+                    if(nr<0||nr>=m||nc<0||nc>=n)
+                        continue;
+                    if(mat[nr][nc]==1 && res[nr][nc]==-1){
+                        queue.offer(new int[]{nr,nc});
+                        res[nr][nc]=d;
+                    }
                 }
             }
+            d++;
         }
-        
-        return mat;
+        return res;
     }
 }
