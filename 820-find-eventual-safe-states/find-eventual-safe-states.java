726cce3f8f20b486845b1008166761deb34bee40
imazjav0017 <imazjaveed@gmail.com>
2026-08-02 20:49:32 +0100

    Time: 4 ms (99.61%) | Memory: 63.7 MB (25.13%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,75 +1,46 @@
 class Solution {
     boolean dfs(
-    int num,
-    List<Integer>[] adj,
-    boolean[] visited,
-    boolean[] rec,
-    boolean[] safeNodes,
-    boolean[] unsafeNodes
-) {
-    if (safeNodes[num]) {
+        int node,
+        int[][] graph,
+        boolean[] onPath,
+        boolean[] safe,
+        boolean[] unsafe
+    ) {
+        if (safe[node]) return true;
+        if (unsafe[node]) return false;
+        if (onPath[node]) return false;
+
+        onPath[node] = true;
+
+        for (int next : graph[node]) {
+            if (!dfs(next, graph, onPath, safe, unsafe)) {
+                onPath[node] = false;
+                unsafe[node] = true;
+                return false;
+            }
+        }
+
+        onPath[node] = false;
+        safe[node] = true;
+
         return true;
     }
 
-    if (unsafeNodes[num]) {
-        return false;
-    }
-
-    if (rec[num]) {
-        return false;
-    }
-
-    visited[num] = true;
-    rec[num] = true;
-
-    for (int child : adj[num]) {
-        boolean isSafe = dfs(
-            child,
-            adj,
-            visited,
-            rec,
-            safeNodes,
-            unsafeNodes
-        );
-
-        if (!isSafe) {
-            rec[num] = false;
-            unsafeNodes[num] = true;
-            return false;
-        }
-    }
-
-    rec[num] = false;
-    safeNodes[num] = true;
-
-    return true;
-}
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        boolean[]terminalNodes=new boolean[n];
-        List<Integer>safeNodes=new ArrayList<>();
-        boolean[]unsafeNodes=new boolean[n];
-        List<Integer>adj[]=new ArrayList[n];
-        for(int i=0;i<n;i++){
-            adj[i]=new ArrayList<>();
-        }
-        for(int i=0;i<n;i++){
-            for(int edge:graph[i]){
-                adj[i].add(edge);
-            }
-            if(graph[i].length==0)
-                terminalNodes[i]=true;
-        }
-        for(int i=0;i<n;i++){
-            if(terminalNodes[i])
-                safeNodes.add(i);
-            else{
-                boolean isSafe=dfs(i,adj,new boolean[n],new boolean[n],terminalNodes,unsafeNodes);
-                if(isSafe){
-                    safeNodes.add(i);
-                }
+        int n = graph.length;
+
+        boolean[] onPath = new boolean[n];
+        boolean[] safe = new boolean[n];
+        boolean[] unsafe = new boolean[n];
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int node = 0; node < n; node++) {
+            if (dfs(node, graph, onPath, safe, unsafe)) {
+                result.add(node);
             }
         }
-        return safeNodes;
+
+        return result;
     }
 }
