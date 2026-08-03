be7dd29853412b464da2efab660a46c10b989535
imazjav0017 <imazjaveed@gmail.com>
2026-08-03 15:40:14 +0100

    Time: 1 ms (86.60%) | Memory: 47.4 MB (31.80%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,33 +1,38 @@
 class Solution {
-    void dfs(int node, List<Integer>[]graph,boolean[]visited){
-        visited[node]=true;
-        for(int child:graph[node]){
-            if(!visited[child]){
-                dfs(child,graph,visited);
-            }
+    int findParent(int x,int[]parent){
+        if(parent[x]!=x){
+            parent[x]=findParent(parent[x],parent);
         }
+        return parent[x];
+    }
+    boolean union(int a,int b, int[]parent,int[]rank){
+        int pa=findParent(a,parent),pb=findParent(b,parent);
+        if(pa==pb)
+            return false;
+        if(rank[pa]<rank[pb]){
+            parent[pa]=pb;
+        }
+        else if(rank[pa]>rank[pb]){
+            parent[pb]=pa;
+        }
+        else{
+            parent[pb]=pa;
+            rank[pa]++;
+        }
+        return true;
     }
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-        List<Integer>[]graph=new ArrayList[n];
+        int[]parents=new int[n];
+        int[]ranks=new int[n];
         for(int i=0;i<n;i++){
-            graph[i]=new ArrayList<>();
+            parents[i]=i;
         }
+        int count=n;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j)
-                    continue;
-                if(isConnected[i][j]==1){
-                    graph[i].add(j);
-                }
-            }
-        }
-        boolean[]visited=new boolean[n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                count++;
-                dfs(i,graph,visited);
+            for(int j=i;j<n;j++){
+                if(isConnected[i][j]==1 && union(i,j,parents,ranks))
+                    count--;
             }
         }
         return count;
