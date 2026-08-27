8368509dc33455ca33e5efd6754c5e3569543254
imazjav0017 <imazjaveed@gmail.com>
2026-08-27 10:08:12 +0100

    Time: 0 ms (100.00%) | Memory: 43.9 MB (62.79%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int l=0,r=m-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(matrix[mid][0]==target) return true;
-            else if(matrix[mid][0]<target){
-                l=mid+1;
-            }
-            else {
-                r=mid-1;
-            }
-        }
-        if(r>=0)l--;
-        int l1=0;
-        r=n-1;
-        while(l1<=r){
-            int mid=(l1+r)/2;
-            if(matrix[l][mid]==target)return true;
-            if(matrix[l][mid]<target){
-                l1=mid+1;
+        int m=matrix.length,n=matrix[0].length;
+        int left=0,right=(m*n)-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int midR=mid/n;
+            int midC=mid%n;
+            if(matrix[midR][midC]==target)
+                return true;
+            else if(matrix[midR][midC]<target){
+                left=mid+1;
             }
             else{
-                r=mid-1;
+                right=mid-1;
             }
         }
         return false;
