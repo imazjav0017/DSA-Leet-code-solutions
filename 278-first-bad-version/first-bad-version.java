2ea2f55c59eb3723207a129dd3eb94d90364436d
imazjav0017 <imazjaveed@gmail.com>
2026-08-27 11:28:24 +0100

    Time: 13 ms (99.88%) | Memory: 42.1 MB (36.06%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -3,17 +3,16 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int lo=1;
-        int hi=n;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(!isBadVersion(mid)){
-                lo=mid+1;
+        int left=0,right=n;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(isBadVersion(mid)){
+                right=mid;
             }
             else{
-                hi=mid-1;
+                left=mid+1;
             }
         }
-        return lo;
+        return left;
     }
 }
