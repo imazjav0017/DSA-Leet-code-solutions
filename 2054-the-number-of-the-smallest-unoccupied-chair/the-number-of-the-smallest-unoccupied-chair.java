d72d5d712815173494747afdc57aedc2b4e42662
imazjav0017 <imazjaveed@gmail.com>
2026-09-08 19:29:07 +0100

    Time: 47 ms (75.55%) | Memory: 51.6 MB (70.53%) - LeetSync

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int smallestChair(int[][] times, int targetFriend) {
+        int targetTime=0;
+        for(int i=0;i<times.length;i++){
+            if(i==targetFriend){
+                targetTime=times[i][0];
+                break;
+            }
+        }
+        Arrays.sort(times,(a,b)->Integer.compare(a[0],b[0]));
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        //[Leaving time, chair]
+        PriorityQueue<int[]>heap=new PriorityQueue<>((a,b)->{
+            if(a[0]!=b[0])
+                return Integer.compare(a[0],b[0]);
+            return Integer.compare(a[1],b[1]);
+        });
+        for(int i=0;i<times.length;i++){
+            pq.offer(i);
+        }
+        for(int[]time:times){
+            while(!heap.isEmpty() && heap.peek()[0]<=time[0]){
+                int[]left=heap.poll();
+                pq.offer(left[1]);
+            }
+            int chair=pq.poll();
+            if(time[0]==targetTime)
+                return chair;
+            heap.offer(new int[]{time[1],chair});
+        }
+        return 0;
+    }
+}
