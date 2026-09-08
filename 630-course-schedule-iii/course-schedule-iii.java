adb418bf8460d62bc65647073c3381eaa370f202
imazjav0017 <imazjaveed@gmail.com>
2026-09-08 21:27:11 +0100

    Time: 50 ms (33.17%) | Memory: 57.5 MB (46.34%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.java
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -1,16 +1,34 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        int n=courses.length;
-        Arrays.sort(courses,(a,b)->Integer.compare(a[1],b[1]));
-        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
-        int totalTime=0;
-        for(int[]c:courses){
-            totalTime+=c[0];
-            pq.offer(c[0]);
-            if(totalTime>c[1]){
-                totalTime-=pq.poll();
+
+        // Earliest deadline first
+        Arrays.sort(courses, (a,b) ->
+            Integer.compare(a[1], b[1])
+        );
+
+        // Selected course durations
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>(Collections.reverseOrder());
+
+        int totalTime = 0;
+
+        for (int[] course : courses) {
+
+            int duration = course[0];
+            int deadline = course[1];
+
+            // Temporarily take course
+            totalTime += duration;
+            pq.offer(duration);
+
+            // Schedule impossible
+            if (totalTime > deadline) {
+
+                // Sacrifice longest course
+                totalTime -= pq.poll();
             }
         }
+
         return pq.size();
     }
 }
