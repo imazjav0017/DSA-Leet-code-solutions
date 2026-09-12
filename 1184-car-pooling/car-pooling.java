4dccab4ea87251a93710fcf1983796b48a695f52
imazjav0017 <imazjaveed@gmail.com>
2026-09-12 19:54:45 +0100

    Time: 7 ms (27.81%) | Memory: 46.4 MB (24.77%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        TreeMap<Integer,Integer>map=new TreeMap<>();
+        for(int[]trip:trips){
+            map.put(trip[1],map.getOrDefault(trip[1],0)+trip[0]);
+            map.put(trip[2],map.getOrDefault(trip[2],0)-trip[0]);
+        }
+        int curr=0;
+        for(int location:map.keySet()){
+            curr+=map.get(location);
+            if(curr>capacity)
+                return false;
+        }
+        return true;
+    }
+}
