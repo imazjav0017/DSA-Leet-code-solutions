cbac3b4afecfdd68be73a0bfe2f63505ac811e3c
imazjav0017 <imazjaveed@gmail.com>
2026-09-15 17:25:45 +0100

    Time: 0 ms (100.00%) | Memory: 43.2 MB (12.31%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,43 +1,34 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        ListNode current=dummy;
-        for(int i=1;i<left;i++){
-            current=current.next;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        // 1. Get node immediately before left
+        ListNode before = dummy;
+
+        for (int i = 1; i < left; i++) {
+            before = before.next;
         }
-        ListNode start=current.next;
-        current.next=null;
-        ListNode end=start,prev=null;
-        for(int i=left;i<=right;i++){
-            prev=end;
-            end=end.next;
+
+        // 2. Reverse [left, right]
+        ListNode curr = before.next;
+        ListNode prev = null;
+
+        for (int i = left; i <= right; i++) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        prev.next=null;
-        ListNode x=start;
-        start=reverse(start);
-        current.next=start;
-        x.next=end;
+
+        // 3. Reconnect
+        ListNode oldStart = before.next;
+
+        before.next = prev;
+        oldStart.next = curr;
+
         return dummy.next;
-    }
-    ListNode reverse(ListNode head){
-        ListNode prev=null,curr=head;
-        while(curr!=null){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
     }
 }
