ce2300e00d37f8e22228e1d0f452bc919621d96e
imazjav0017 <imazjaveed@gmail.com>
2026-09-15 11:29:04 +0100

    Time: 1 ms (56.09%) | Memory: 47.9 MB (42.66%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,19 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-        int units=0;
-        Deque<Integer>stack=new ArrayDeque<>();
-        for(int i=0;i<height.length;i++){
-            int h=height[i];
-            while(!stack.isEmpty() && h>height[stack.peek()]){
-                int last=stack.pop();
-                if(!stack.isEmpty()){
-                    int curHeight=Math.min(h,height[stack.peek()])-height[last];
-                    int w=i-stack.peek()-1;
-                    units+=w*curHeight;
-                }
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left <= right) {
+
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if (leftMax <= rightMax) {
+
+                water += leftMax - height[left];
+                left++;
+
+            } else {
+
+                water += rightMax - height[right];
+                right--;
             }
-            stack.push(i);
         }
-        return units;
+
+        return water;
     }
 }
