9b58e2db41fb1301420c802d7f60a9e76cc3d2f5
imazjav0017 <imazjaveed@gmail.com>
2026-09-16 17:39:13 +0100

    Time: 20 ms (86.24%) | Memory: 66.5 MB (51.20%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,25 +1,21 @@
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
         int n=nums.length;
-        int ans=n+1;
-        long[]prefix=new long[n+1];
-        long sum=0;
-        for(int i=0;i<n;i++){
-            prefix[i]=sum;
-            sum+=nums[i];
-        }
-        prefix[n]=sum;
+        int minLen=n+1;
         Deque<Integer>dq=new ArrayDeque<>();
-        dq.offer(0);
+        long[]prefix=new long[n+1];
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i]+nums[i];
+        }
         for(int i=0;i<=n;i++){
             while(!dq.isEmpty() && prefix[i]-prefix[dq.peekFirst()]>=k){
-                ans=Math.min(ans,i-dq.pollFirst());
+                minLen=Math.min(minLen,i-dq.pollFirst());
             }
-            while(!dq.isEmpty() && prefix[dq.peekLast()]>=prefix[i]){
+            while(!dq.isEmpty() && prefix[i]<=prefix[dq.peekLast()]){
                 dq.pollLast();
             }
             dq.offerLast(i);
         }
-        return ans==n+1?-1:ans;
+        return minLen==n+1?-1:minLen;
     }
 }
