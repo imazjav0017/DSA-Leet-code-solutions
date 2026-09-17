92e9d4cb2497418786170566f39fbd64dba34f7a
imazjav0017 <imazjaveed@gmail.com>
2026-09-17 19:56:58 +0100

    Time: 0 ms (100.00%) | Memory: 43.4 MB (79.33%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,29 +1,45 @@
 class Solution {
-    Map<Integer,char[]> map=new HashMap<>();
+
+    String[] map = {
+        "", "", "abc", "def",
+        "ghi", "jkl", "mno",
+        "pqrs", "tuv", "wxyz"
+    };
+
     public List<String> letterCombinations(String digits) {
-        List<String>res=new ArrayList<>();
-        map.put(2,new char[]{'a','b','c'});
-        map.put(3,new char[]{'d','e','f'});
-        map.put(4,new char[]{'g','h','i'});
-        map.put(5,new char[]{'j','k','l'});
-        map.put(6,new char[]{'m','n','o'});
-        map.put(7,new char[]{'p','q','r','s'});
-        map.put(8,new char[]{'t','u','v'});
-        map.put(9,new char[]{'w','x','y','z'});
-        StringBuilder path=new StringBuilder();
-        backtrack(digits,0,path,res);
+
+        List<String> res = new ArrayList<>();
+
+        if (digits.length() == 0)
+            return res;
+
+        StringBuilder path = new StringBuilder();
+
+        backtrack(digits, 0, path, res);
+
         return res;
     }
-    void backtrack(String digits, int index, StringBuilder path, List<String>res){
-        if(index==digits.length()){
+
+    void backtrack(String digits, int index,
+                   StringBuilder path, List<String> res) {
+
+        if (index == digits.length()) {
             res.add(path.toString());
             return;
         }
-        char[]options=map.get(digits.charAt(index)-'0');
-        for(char c:options){
+
+        String options = map[digits.charAt(index) - '0'];
+
+        for (char c : options.toCharArray()) {
+
+            // CHOOSE
             path.append(c);
-            backtrack(digits,index+1,path,res);
-            path.deleteCharAt(path.length()-1);
+
+            // EXPLORE
+            backtrack(digits, index + 1, path, res);
+
+            // UNDO
+            path.deleteCharAt(path.length() - 1);
         }
     }
 }
