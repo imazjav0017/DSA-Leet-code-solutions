eadc688478c95a8603a3359fe358ee967786f7aa
imazjav0017 <imazjaveed@gmail.com>
2026-09-17 21:27:18 +0100

    Time: 0 ms (100.00%) | Memory: 42.7 MB (74.82%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -5,12 +5,13 @@ class Solution {
             return nums[0];
         if(n==2)
             return Math.max(nums[0],nums[1]);
-        int[]dp=new int[n];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
+       int prev2=nums[0];
+    int prev1=Math.max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+            int current=Math.max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=current;
         }
-        return dp[n-1];
+        return prev1;
     }
 }
