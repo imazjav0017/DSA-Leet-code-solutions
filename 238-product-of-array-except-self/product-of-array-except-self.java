fa7d0401a453fab39052836a5b5345bce1480e56
imazjav0017 <imazjaveed@gmail.com>
2026-09-25 11:43:48 +0100

    Time: 2 ms (92.49%) | Memory: 72 MB (55.78%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int[]answer=new int[n];
-        int prefix=1;
-        for(int i=0;i<n;i++){
-            answer[i]=prefix;
-            prefix*=nums[i];
-        }
-        int suffix=1;
-        for(int i=n-1;i>=0;i--){
-            answer[i]*=suffix;
-            suffix*=nums[i];
-        }
-        return answer;
+     int n=nums.length;
+     int prefix=1;
+     int[]res=new int[n];
+     for(int i=0;i<n;i++){
+        res[i]=prefix;
+        prefix=prefix*nums[i];
+     }
+     int suffix=1;
+     for(int i=n-1;i>=0;i--){
+        res[i]=res[i]*suffix;
+        suffix=suffix*nums[i];
+     }
+     return res;
     }
 }
