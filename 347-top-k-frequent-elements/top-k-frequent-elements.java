baa635363d930f8b9f30212fd63a34c6959b6c94
imazjav0017 <imazjaveed@gmail.com>
2026-09-25 11:36:44 +0100

    Time: 15 ms (55.11%) | Memory: 47.6 MB (66.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer>freq=new HashMap<>();
-        for(int i: nums){
-            freq.put(i,freq.getOrDefault(i,0)+1);
+       Map<Integer,Integer>freq=new HashMap<>();
+       for(int i:nums){
+        freq.put(i,freq.getOrDefault(i,0)+1);
+       }
+       PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->Integer.compare(freq.get(a),freq.get(b)));
+       for(int i:freq.keySet()){
+        pq.offer(i);
+        if(pq.size()>k){
+            pq.poll();
         }
-        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->
-        Integer.compare(freq.get(a),freq.get(b)));
-        for(int key:freq.keySet()){
-            pq.offer(key);
-            if(pq.size()>k)
-                pq.poll();
-        }
-        int[]res=new int[k];
-        for(int i=0;i<k;i++){
-            res[i]=pq.poll();
-        }
-        return res;
+       }
+       int[]res=new int[k];
+       int idx=0;
+       while(!pq.isEmpty()){
+        res[idx++]=pq.poll();
+       }
+       return res;
     }
 }
